b167a8fd5fe345a3e6ea333ad069d3bf604f15fa
silentsniper0818 <silentsniper0818@gmail.com>
2025-11-13 22:00:44 +0100

    Update config.js

--- config.js
+++ config.js
@@ -25,13 +25,13 @@ module.exports = {
   BLOCKCHAT: process.env.BLOCK_CHAT || false,
   LANG: (process.env.LANGUAGE || 'EN').toUpperCase(),
   ALWAYS_ONLINE: toBool(process.env.ALWAYS_ONLINE),
-  BOT_NAME: process.env.BOT_NAME || 'ʜᴇʀᴍɪᴛ',
+  BOT_NAME: process.env.BOT_NAME || 'BENZENE BOT',
   AUTOMUTE_MSG: process.env.AUTOMUTE_MSG || '_Group automuted!_\n_(Change this by setting var AUTOMUTE_MSG)_',
   AUTOUNMUTE_MSG: process.env.AUTOUNMUTE_MSG || '_Group autounmuted!_\n_(Change this by setting var AUTOUNMUTE_MSG)_',
   ANTILINK_MSG: process.env.ANTILINK_MSG || '_Link Not Allowed!_\n_(Change this by setting var ANTILINK_MSG)_',
-  BOT_INFO: process.env.BOT_INFO || 'ʜᴇʀᴍɪᴛ;ᴀᴅɪᴛʜyᴀɴ;972528277755;https://i.imgur.com/6oRG106.jpeg',
+  BOT_INFO: process.env.BOT_INFO || 'BENZENE; BENEDICT;2347067359270;https://i.imgur.com/6oRG106.jpeg',
   AUDIO_DATA: process.env.AUDIO_DATA || 'ʜᴇʀᴍɪᴛ;ᴀᴅɪᴛʜyᴀɴ;https://i.imgur.com/fj2WE83.jpeg',
-  STICKER_DATA: process.env.STICKER_DATA || 'ʜᴇʀᴍɪᴛ;ᴀᴅɪᴛʜyᴀɴ',
+  STICKER_DATA: process.env.STICKER_DATA || 'BENZENE',
   ERROR_MESSAGE: toBool(process.env.ERROR_MESSAGE, 'true'),
   SONG_THUMBNAIL: toBool(process.env.SONG_THUMBNAIL),
   WARN: process.env.WARN || '4',
@@ -66,4 +66,5 @@ module.exports = {
   BRAIN_ID: process.env.BRAIN_ID || 'bid=168613&key=EfbnX54Iy9PFIFp3',
   SUDO: process.env.SUDO || '0,0',
   DEBUG: DEBUG
+
 };
